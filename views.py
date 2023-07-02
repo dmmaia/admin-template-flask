e3168755aff062b054b0d930d3634f5c46382220
dmmaia <david_mmaia@hotmail.com>
2023-07-02 11:33:20 -0300

    users list page added

--- views.py
+++ views.py
@@ -5,3 +5,7 @@ views = Blueprint(__name__,"views")
 @views.route("/")
 def home():
     return render_template("dashboard.html")
+
+@views.route("/users")
+def users():
+    return render_template("users.html")
